3d7d33f07d801065f8293d4d98e2cf42ad802518
Joel Olawanle <57611810+olawanlejoel@users.noreply.github.com>
2023-07-27 11:04:45 +0100

    Update FormSection.test.jsx

--- src/components/FormSection.test.jsx
+++ src/components/FormSection.test.jsx
@@ -6,7 +6,7 @@ import FormSection from './FormSection';
 describe('FormSection', () => {
     test('renders the textarea and button', () => {
         render(<FormSection />);
-        const textareaElement = screen.getByPlaceholderText('Ask me anything...');
+        const textareaElement = screen.getByPlaceholderText('Ask me anyting...');
         const buttonElement = screen.getByText('Generate Response 🤖');
 
         expect(textareaElement).toBeInTheDocument();
